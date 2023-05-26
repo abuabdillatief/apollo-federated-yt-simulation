580024203410cc3fbf7cd500185367323cb39fd8
Mohammad Rendra Sura Aditama <61033485+abuabdillatief@users.noreply.github.com>
2023-05-26 14:07:11 +0700

    enhance standardized rmq messave class to include token

--- libs/common/src/rmq/rmq.message.ts
+++ libs/common/src/rmq/rmq.message.ts
@@ -1,9 +1,11 @@
 import { RmqContext } from "@nestjs/microservices"
 
 export class RmqMessageValue<T> {
-    constructor({ value }: { value: T }) {
+    constructor({ value, token }: { value: T, token: string }) {
         this.value = value
+        this.token = token
     }
     value: T
     context: RmqContext
+    token: string
 }
