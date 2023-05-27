62490cc775eab7a8ad1ec91ccaf177fd737dfa6b
Mohammad Rendra Sura Aditama <61033485+abuabdillatief@users.noreply.github.com>
2023-05-27 10:55:29 +0700

    enhance rmq module message constructor

--- libs/common/src/rmq/rmq.message.ts
+++ libs/common/src/rmq/rmq.message.ts
@@ -1,7 +1,7 @@
 import { RmqContext } from "@nestjs/microservices"
 
 export class RmqMessageValue<T> {
-    constructor({ value, token }: { value: T, token: string }) {
+    constructor({ value, token }: { value: T, token?: string }) {
         this.value = value
         this.token = token
     }
